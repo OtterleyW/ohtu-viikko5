bd5f3b64d79e5ff7d4cc22794ba8d27e26392500
Jenni <jenniknurmi@gmail.com>
2017-12-01 18:17:31 +0200

    Sovellusta Tennis refaktoroitu (tehtävä 2)

--- Tennis/src/main/java/ohtu/TennisGame.java
+++ Tennis/src/main/java/ohtu/TennisGame.java
@@ -1,9 +1,9 @@
 package ohtu;
 
 public class TennisGame {
-    
-    private int m_score1 = 0;
-    private int m_score2 = 0;
+
+    private int player1_score = 0;
+    private int player2_score = 0;
     private String player1Name;
     private String player2Name;
 
@@ -13,68 +13,94 @@ public class TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
-        else
-            m_score2 += 1;
+        if (playerName == this.player1Name) {
+            player1_score += 1;
+        } else {
+            player2_score += 1;
+        }
     }
 
     public String getScore() {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
+        int tempScore = 0;
+        if (player1_score == player2_score) {
+           score = whenGameIsEven(player1_score);
+        } else if (player1_score >= 4 || player2_score >= 4) {
+           score = whenOtherPlayerIsLeadingOverFour(player1_score, player2_score);
+        } else {
+           score = whenOtherSituationInGame(tempScore);
+        }
+        return score;
+    }
+
+    private String whenGameIsEven(int player1_score) {
+        String score = "";
+        
+        switch (player1_score) {
+            case 0:
+                score = "Love-All";
+                break;
+            case 1:
+                score = "Fifteen-All";
+                break;
+            case 2:
+                score = "Thirty-All";
+                break;
+            case 3:
+                score = "Forty-All";
+                break;
+            default:
+                score = "Deuce";
+                break;
+
+        }
+        
+        return score;
+    }
+    
+    private String whenOtherPlayerIsLeadingOverFour(int player1_score, int player2_score){
+        String score = "";
+        
+        int minusResult = player1_score - player2_score;
+        if (minusResult == 1) {
+            score = "Advantage player1";
+        } else if (minusResult == -1) {
+            score = "Advantage player2";
+        } else if (minusResult >= 2) {
+            score = "Win for player1";
+        } else {
+            score = "Win for player2";
+        }
+        
+        return score;
+    }
+    
+    private String whenOtherSituationInGame(int tempScore){
+        String score = "";
+        
+        for (int i = 1; i < 3; i++) {
+            if (i == 1) {
+                tempScore = player1_score;
+            } else {
+                score += "-";
+                tempScore = player2_score;
+            }
+            switch (tempScore) {
                 case 0:
-                        score = "Love-All";
+                    score += "Love";
                     break;
                 case 1:
-                        score = "Fifteen-All";
+                    score += "Fifteen";
                     break;
                 case 2:
-                        score = "Thirty-All";
+                    score += "Thirty";
                     break;
                 case 3:
-                        score = "Forty-All";
+                    score += "Forty";
                     break;
-                default:
-                        score = "Deuce";
-                    break;
-                
             }
         }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
-        }
+        
         return score;
     }
 }
